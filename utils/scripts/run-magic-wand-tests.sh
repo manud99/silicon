61cb01fb6c2fbde672f1cc8acca96941318f1854
Manuel Dublanc <19774382+manud99@users.noreply.github.com>
2024-05-06 09:37:52 +0200

    Add file to run all tests that include a magic wand.

---
+++ utils/scripts/run-magic-wand-tests.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Run all tests that include a magic wand.
+# Recommended Usage:
+# bash utils/scripts/run-magic-wand-tests.sh silver/src/test/resources/
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <tests-folder>"
+  exit 1
+fi
+
+FOLDER=$1
+
+if [ ! -d "$FOLDER" ]; then
+  echo "Folder $FOLDER does not exist"
+  exit 1
+fi
+
+# Get all tests that include a magic wand
+tests=$(grep -rnwl "$FOLDER" -e '--\*' --include "*.vpr" --exclude-dir={graphs,capture_avoidance,quasihavoc,transformations,adt})
+
+# Split the tests into an array
+tests=($tests)
+
+# Remove the value from $FOLDER from the tests
+for i in "${!tests[@]}"; do
+  tests[$i]=$(echo ${tests[$i]} | sed "s|$FOLDER||")
+done
+
+# Construct the command to run the tests
+tests=$(printf " -n %s" "${tests[@]}")
+
+# Run the tests
+echo "sbt \"testOnly -- $tests\""
+sbt "testOnly -- $tests"
